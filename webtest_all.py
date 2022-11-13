b068645a768568809a0782de274d1f6d4b7cac3d
Ding Zhang <dingzhang.ds@gmail.com>
2022-11-13 15:47:27 -0700

    update pytest ini web test workflow file

--- webtest_all.py
+++ webtest_all.py
@@ -2,12 +2,10 @@
 # -*-coding:utf-8-*-
 # @Time: 2022-11-10 3:10 p.m.
 # Author: Ding
-import os
-import time
+# import os
 
 import pytest
 
 if __name__ == '__main__':
     pytest.main()
-    time.sleep(1)
-    os.system("allure generate report/ -o report/html --clean")
+    # os.system("allure generate web_test_report/ -o web_test_report/html --clean")
